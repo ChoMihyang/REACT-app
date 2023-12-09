637bf6903797c83f1c5934d3e2c25343ebb5b434
ChoMihyang <chomh316@gmail.com>
2023-12-10 02:56:51 +0900

    Start Management Project

---
+++ management/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Customer from "./components/Customer";
+import "./App.css";
+
+function App() {
+  return <Customer />;
+}
+
+export default App;
---
+++ management/src/components/Customer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Customer() {
+  return (
+    <div>
+      <h2>홍길동</h2>
+      <p>961222</p>
+      <p>남자</p>
+      <p>대학생</p>
+    </div>
+  );
+}
+export default Customer;
