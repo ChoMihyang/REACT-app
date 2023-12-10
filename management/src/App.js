d342978f78df7f2d8daaa617398e66831e177696
ChoMihyang <chomh316@gmail.com>
2023-12-11 00:35:04 +0900

    Create Customer.js

--- management/src/App.js
+++ management/src/App.js
@@ -2,21 +2,49 @@ import React from "react";
 import Customer from "./components/Customer";
 import "./App.css";
 
-const customer = {
-  name: "田中",
-  birthday: "961222",
-  gender: "男",
-  gob: "大学生",
-};
+const customers = [
+  {
+    id: 1,
+    image: "https://i.pravatar.cc/200​​",
+    name: "田中",
+    birthday: "960101",
+    gender: "男",
+    job: "大学生",
+  },
+  {
+    id: 2,
+    image: "https://i.pravatar.cc/200​​",
+    name: "鈴木",
+    birthday: "960102",
+    gender: "女",
+    job: "プログラマー",
+  },
+  {
+    id: 3,
+    image: "https://i.pravatar.cc/200​​",
+    name: "野口",
+    birthday: "960103",
+    gender: "男",
+    job: "営業",
+  },
+];
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.gob}
-    />
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            job={c.job}
+          />
+        );
+      })}
+    </div>
   );
 }
 
--- management/src/components/Customer.js
+++ management/src/components/Customer.js
@@ -1,17 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Customer({ name, birthday, gender, job }) {
+// 한 명의 고객에 대한 정보를 출력
+// 一人の顧客についての情報を出力
+function Customer({ id, image, name, birthday, gender, job }) {
   return (
     <div>
-      <h2>{name}</h2>
+      <CustomerProfile id={id} image={image} name={name} />
+      <CustomerInfo birthday={birthday} gender={gender} job={job} />
+    </div>
+  );
+}
+
+// CustomerProfile: id, image, name
+function CustomerProfile({ id, image, name }) {
+  return (
+    <div>
+      <img src={image} alt="profile" />
+      <h2>
+        {name}({id})
+      </h2>
+    </div>
+  );
+}
+
+// CustomerInfo: birthday, gender, job
+function CustomerInfo({ birthday, gender, job }) {
+  return (
+    <div>
       <p>{birthday}</p>
       <p>{gender}</p>
       <p>{job}</p>
     </div>
   );
 }
+
 Customer.prototype = {
+  id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   birthday: PropTypes.number.isRequired,
   gender: PropTypes.string.isRequired,
