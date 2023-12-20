d45e1f0237580bffb9867e3497edab680d137316
ChoMihyang <chomh316@gmail.com>
2023-12-20 23:56:40 +0900

    Install CORS

--- management/client/src/components/CustomerAdd.js
+++ management/client/src/components/CustomerAdd.js
@@ -15,7 +15,7 @@ import {
 } from "@mui/material";
 import axios from "axios";
 
-// const axios = axios.create({ baseURL: "http://localhost:4000" });
+// const instance = axios.create({ baseURL: "http://localhost:4000/customers" });
 
 function CustomerAdd() {
   //                  const event
--- management/server/src/index.js
+++ management/server/src/index.js
@@ -3,28 +3,33 @@
 
 // expressモジュールをインポートしてExpressアプリケーションを作成する
 const express = require("express");
-
 const mongoose = require("mongoose");
-
 // Expressオブジェクト作成
 const app = express();
 const port = 4000;
-
+const cors = require('cors');
 const dotenv = require("dotenv");
-dotenv.config();
 const db = process.env.MONGO_URI;
+
+dotenv.config();
 mongoose
   .connect(db)
   .then(() => {
     console.log("연결 완료");
   })
   .catch((error) => console.log(error));
+ 
+app.use(cors());
+app.use(express.json());
 
 // あるルートにアクセスされた際に表示する処理（res）を指定する
 // app.get("/", (req, res) => {
 //   res.send("Hello, world!");
 // });
-
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+})
 app.use("/customers", require("./routes/customers"));
 
 // 指定したポートにアクセスする
